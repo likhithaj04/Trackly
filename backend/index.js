eb52da33b78ba160ba2c340d3c9b2143b723ec6e
likhithaj04 <likhitha15jayram@gmail.com>
2026-05-13 21:29:15 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,39 @@
-const express=require('express')
-const app=express()
-require('dotenv').config()
-const Job=require('./model/jobSchema')
-const mongoose=require('mongoose')
-const auth=require('./route/auth')
-const cors=require('cors');
-const userRouter=require('./route/user')
-const cookieParser=require('cookie-parser')
+const express = require('express');
+const app = express();
 
-const jobdataRouter=require('./route/jobdata')
+require('dotenv').config();
 
-app.use(express.json())
-app.use(cookieParser())
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const auth = require('./route/auth');
+const userRouter = require('./route/user');
+const jobdataRouter = require('./route/jobdata');
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use("/", userRouter);
+app.use("/auth", auth);
+app.use("/job", jobdataRouter);
+
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send("Something went wrong");
 });
 
-app.use(cors({origin:'http://localhost:5173',
-    credentials:true
-}))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected");
 
-app.use("/",userRouter)
-app.use("/auth",auth)
-app.use("/job",jobdataRouter)
-
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-//  limits: { fileSize: 5 * 1024 * 1024 }
-// });
-
-
-app.listen((8000),(req,res)=>{
-    console.log("Server started");
-    mongoose.connect(process.env.Mongo_uri).then(()=>console.log("Mongodb connected"))
+    app.listen(8000, () => {
+        console.log("Server started");
+    });
 })
+.catch(err => console.log(err));
