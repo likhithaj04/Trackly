20243f8852ca953ab33300b794c35ed747492174
likhithaj04 <likhitha15jayram@gmail.com>
2026-05-13 21:52:37 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'https://trackly-alpha-silk.vercel.app/',
+    origin: 'https://trackly-alpha-silk.vercel.app',
     credentials: true
 }));
 
